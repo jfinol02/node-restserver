ec01a5b7318c00a740309523795886ed201f2769
Jesus Finol <jfinol02@gmail.com>
2020-06-04 13:14:21 -0400

    Se agrego la BD que no estaba en la URL

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = "mongodb+srv://jfinol02:Kv3g9SE1DRMpIlgl@cluster0-v2pxs.mongodb.net/<dbname>?retryWrites=true&w=majority";
+    urlDB = "mongodb+srv://jfinol02:Kv3g9SE1DRMpIlgl@cluster0-v2pxs.mongodb.net/cafe?retryWrites=true&w=majority";
 }
 
 process.env.URLDATABASE = urlDB;
